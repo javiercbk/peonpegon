ae9e9741a77662fa89642d67d410647e7cd71026
mdellapittima <mdellapittima@13cb0d4a-4091-e191-394b-99c563990401>
2010-10-31 03:58:14 +0000

    version 1

---
+++ Aplicacion Desktop/CalificacionBancariaDesktop/CalificacionBancariaDesktop/OtorgacionPrestamos/FormOtorgacionPrestamos.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalificacionBancariaDesktop.OtorgacionPrestamos
+{
+    public partial class FormOtorgacionPrestamos : Form
+    {
+        public FormOtorgacionPrestamos()
+        {
+            InitializeComponent();
+        }
+    }
+}
